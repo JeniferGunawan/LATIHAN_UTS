de9d3d9428887aad694ed6487bb6330bcf594236
Michael <jenifergunawan5@gmail.com>
2024-10-30 13:26:01 +0700

    update code - penambahan mvc

--- LATIHAN_UTS/app.js
+++ LATIHAN_UTS/app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-require('./models/db');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var produksRouter = require('./routes/produks');
+
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+require('./app_server/models/db');
+var produksRouter = require('./app_server/routes/produks');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', produksRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
